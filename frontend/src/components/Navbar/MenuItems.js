cbb6c282e35571b1de5fc755826cc89edffbee33
JGamez17 <github email address>
2021-01-29 16:25:02 -0500

    WIP: navbar complete

---
+++ frontend/src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Recipes",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "New Recipe",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+  },
+];
---
+++ frontend/src/components/Recipe.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+const Recipe = () => {
+  return <div></div>;
+};
