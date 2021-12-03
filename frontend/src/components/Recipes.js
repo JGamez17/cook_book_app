5991c7f034084b7ebe528c004b5f6d7b053af951
JGamez17 <github email address>
2021-12-03 16:36:10 -0500

    WIP: rendered recipes from API

--- frontend/src/components/Recipes.js
+++ frontend/src/components/Recipes.js
@@ -11,22 +11,19 @@ class Recipes extends React.Component {
     this.setState({ recipeSearch: e.target.value })
   }
 
-  filterSearch = () => {
-    return this.state.recipeSearch.filter(recipe => recipe.toLowerCase().includes(this.state.recipeSearch.toLowerCase()))
-  }
-
   render() {
     return (
       <div className="recipes">
         <label> Search By Recipe </label>
         <input type='text' value={this.state.recipeSearch} onChange={this.recipeSearch} placeholder='Searching...' />
         {this.props.recipes.map((recipe) => {
-          // instead of using this.props.recipe use filter function that will filter recipes and then map
-          // google search - how to use filter js (google small steps)
-          // how to use onChange with input field (BE SPECIFIC)
           return <RecipeCard key={recipe.id} recipeObj={recipe} />;
         })}
 
+        {/* 
+      // instead of using this.props.recipe use filter function that will filter recipes and then map
+      // google search - how to use filter js (google small steps)
+      // how to use onChange with input field (BE SPECIFIC) */}
       </div>
     );
   }
