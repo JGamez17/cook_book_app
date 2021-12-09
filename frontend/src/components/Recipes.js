2369b43fb88b381d9c19398f71b7e6f13c53d4ec
JGamez17 <github email address>
2021-12-08 21:18:59 -0500

    Added search and filter function

--- frontend/src/components/Recipes.js
+++ frontend/src/components/Recipes.js
@@ -3,42 +3,42 @@ import RecipeCard from "../components/RecipeCard";
 import { connect } from "react-redux";
 
 class Recipes extends React.Component {
+  // this.state.recipe
   state = {
     recipe: ''
   };
 
+
   handleOnChange = (e) => {
-    // console.log('working input')
     this.setState({
       recipe: e.target.value
     })
   }
 
-  filterRecipe = () => {
-    return this.props.recipes.filter(function (recipeObj) {
-      if (recipeObj.name === recipeSearchFor) {
-        return true;
-      }
-      return false;
-    });
+  // filterRecipes = () => {
+  //   const recipeMatches = this.props.recipes.filter(function (recipeObj) {
 
-  }
+  //     if (recipeObj.title === this.state.recipe) {
+  //       return true;
+  //     }
+
+  //     return false;
+  //   });
+  // }
+
   render() {
+    console.log(this.state)
     return (
       <div className="recipes">
         <form>
           <label> Search By Title </label>
           <input type='text'
             onChange={this.handleOnChange} placeholder='Searching...' />
-          {this.props.recipes.filter(recipeObj => recipeObj.title.includes(this.state.recipe)).map(filteredRecipeObj => (
-            <RecipeCard Key={filteredRecipeObjs.id} recipeObj={filteredRecipeObj} />
-            //  return <RecipeCard key={recipe.id} recipeObj={recipe} />;
+          {/* {this.filterRecipes()} */}
+          {this.props.recipes.filter(recipeObj => recipeObj.title.toLowerCase().includes(this.state.recipe.toLowerCase())).map(filteredRecipeObj => (
+            <RecipeCard Key={filteredRecipeObj.id} recipeObj={filteredRecipeObj} />
           ))}
 
-          {/* 
-      // instead of using this.props.recipe use filter function that will filter recipes and then map
-      // google search - how to use filter js (google small steps)
-      // how to use onChange with input field (BE SPECIFIC) */}
         </form>
       </div>
     );
