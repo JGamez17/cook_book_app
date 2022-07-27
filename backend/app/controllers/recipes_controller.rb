467eb1d6d5c4a4004435fe3a5b683e295b662059
JGamez17 <github email address>
2022-07-27 16:31:56 -0400

    updated recipes controller

--- backend/app/controllers/recipes_controller.rb
+++ backend/app/controllers/recipes_controller.rb
@@ -1,21 +1,32 @@
 class RecipesController < ApplicationController
-    def index
-        recipes = Recipe.all 
+    before_action :set_recipe, only: [:show, :update, :destory]
 
-        render json: recipes
+    def index
+        @recipes = Recipe.all 
+
+        render json: @recipes
      
     end
 
 
     def create
-        recipe = Recipe.new
-            if recipe.save 
-        render json: recipe, status: :created
+        @recipe = Recipe.new(recipe_params)
+           
+        if @recipe.save 
+            render json: @recipe, status: :created, location: @recipes
+        else 
+            render json: @recipe.errors, status: :unprocessable_entity
         end
     end     
 
+
+
     def destroy
-        recipe.destroy
+        @recipe.destroy
     end
+
+    def recipe_params
+        params.require(:recipe).permit(:title, :ingredients, :prep, :directions)
+    end 
 
 end
